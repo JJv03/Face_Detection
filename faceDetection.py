ea8cd4d9c9252d56410cf3ba6ab319f646dd5c75
Juan Valle Morenilla <juanvalle2003@gmail.com>
2024-06-04 22:14:13 +0200

    Eyes and smiles implementation

--- faceDetection.py
+++ faceDetection.py
@@ -3,11 +3,10 @@ import sys # Sys module: access to Python interpreter variables
 
 print("Press \'esc\' to exit the program")
 
-# Get the Haar cascade file path, use the default if none is provided
-cascPath = sys.argv[1] if len(sys.argv) > 1 else 'haarcascade_frontalface_default.xml'
-
-# Create the CascadeClassifier object
-faceCascade = cv2.CascadeClassifier(cascPath)
+# Create the CascadeClassifier objects for the face, eyes and smile detection
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 # Start video capture, 0 refers to the system's default camera
 video_capture = cv2.VideoCapture(0)
@@ -35,10 +34,36 @@ while True:
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
     )
-
+    
     # Draw a rectangle around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        
+        # Eyes detection in the grayscale image
+        eyes = eyeCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=5,
+            minSize=(5, 5),
+            )
+        
+        # Draw a rectangle around the detected eyes
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        
+        # Smile detection in the grayscale image
+        smile = smileCascade.detectMultiScale(
+            roi_gray,
+            scaleFactor= 1.5,
+            minNeighbors=15,
+            minSize=(25, 25),
+            )
+        
+        # Draw a rectangle around the detected smiles
+        for (xx, yy, ww, hh) in smile:
+            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
